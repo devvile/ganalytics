c47fbea7fab78cc72d63bf85a31340281470fa6f
devvile <patryk.czemierowski@gmail.com>
2024-03-18 16:02:48 +0100

    google analytics module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // For forms
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
@@ -8,6 +9,7 @@ import { TileSpaceComponent } from './tile-space/tile-space.component';
 import { TileComponent } from './tile/tile.component';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { GtagModule } from 'angular-gtag';
 
 
 @NgModule({
@@ -20,9 +22,17 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxGoogleAnalyticsModule.forRoot('G-6NV03JS26M'),
+    NgxGoogleAnalyticsRouterModule
   ],
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+//trackingId: 'G-6NV03JS26M'
--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -1,11 +1,15 @@
 import { Input ,Component } from '@angular/core';
+import { Gtag } from 'angular-gtag';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
   styleUrl: './tile.component.css'
 })
+
 export class TileComponent {
+  constructor(){}
+
   @Input() tileData: any;
   buttonText= "Go to Store";
 }
